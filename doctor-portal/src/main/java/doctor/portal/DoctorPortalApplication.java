7ca3e36e88270b65fb0537cc24e70738dae77eb3
dev-ankit-dev <62471092+dev-ankit-dev@users.noreply.github.com>
2020-04-21 14:35:47 +0530

    Add files via upload
    initial commit for doctor service

---
+++ doctor-portal/src/main/java/doctor/portal/DoctorPortalApplication.java
@@ -0,0 +1,17 @@
+package doctor.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class DoctorPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorPortalApplication.class, args);
+	}
+
+}
