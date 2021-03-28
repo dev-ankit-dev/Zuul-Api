676f71ba47eeae3474641a9111a5a7e842f38a47
ankit <ashumca108@gmail.com>
2021-03-28 22:39:38 +0530

    hello

---
+++ src/main/java/org/example/ws/Application.java
@@ -0,0 +1,23 @@
+package org.example.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Spring Boot main Application
+ *
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+        System.out.println("hello how are sagun");
+        System.out.println("hello how are amit");
+        System.out.println("hello how are sagun");
+        System.out.println("hello how are amit");
+
+    }
+}
